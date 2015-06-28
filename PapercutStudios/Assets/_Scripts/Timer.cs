f9bbf2bdc3607a2537cd8ef91de540e45b87b343
Mackenzie Allanson <111294@student.sae.edu.au>
2015-06-28 12:14:52 +1000

    Timer added Time Up scene added Main Menu button added

---
+++ PapercutStudios/Assets/_Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public Text timerLabel;
+	
+	public float CountDownTime;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CountDownTime -= Time.deltaTime;
+		
+		var minutes = CountDownTime / 60;
+		var seconds = CountDownTime % 60;
+		//var fraction = (CountDownTime * 100) % 100;
+
+		timerLabel.text = string.Format ("{0:00} : {1:00}", minutes, seconds);
+
+		if (CountDownTime <= 0.0f) 
+		{
+			Application.LoadLevel(1);
+		}
+	}
+}
