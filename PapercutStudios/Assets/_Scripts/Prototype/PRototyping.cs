89f320e17586f1a2bf4a64ef8446a625c8da2339
Lachlan Brown <lachlan.brown8@live.com.au>
2015-06-30 17:17:41 +1000

    + Made end game scene exist on the original main scene as an empty game object  + Started filling out rules and information  + Rules now tied to timer  + Working on an idea for adding the answers
    Need to fix timer, doesn't count minutes properly

--- PapercutStudios/Assets/_Scripts/Prototype/PRototyping.cs
+++ PapercutStudios/Assets/_Scripts/Prototype/PRototyping.cs
@@ -24,6 +24,11 @@ public class PRototyping : MonoBehaviour {
 
 	public GameObject MainMenu;
 	public GameObject PlayerInfo;
+	public GameObject EndScreen;
+	public GameObject PlayButton;
+	
+	public Text Game1ButtonText;
+	public Text Game2ButtonText;
 
 	public Game Game1;
 	public Game Game2;
@@ -33,15 +38,38 @@ public class PRototyping : MonoBehaviour {
 	public Text[] Items;
 	public Text[] Rules;
 
+
 	private Player SelectedPlayer;
 	private Game ActiveGame;
+	private int iCurrentRules = 0;
+	private Text PlayButtonText;
 
+	void Awake () {
+		PlayButtonText = PlayButton.GetComponentInChildren<Text>();
+	}
+
+	void Start () {
+		PlayButton.SetActive(false);
+		Game2.Player1.Rules = Game1.Player1.Rules;
+		Game2.Player2.Rules = Game1.Player2.Rules;
+		Game2.Player3.Rules = Game1.Player3.Rules;
+		Game2.Player4.Rules = Game1.Player4.Rules;
+		Game1ButtonText.text = "Game 1 (Selected)";
+	}
+
+	#region Main Menu
 	public void ButtonGame1() {
 		ActiveGame = Game1;
+		Game1ButtonText.text = "Game 1 (Selected)";
+		Game2ButtonText.text = "Game 2";
+
 	}
 
 	public void ButtonGame2() {
 		ActiveGame = Game2;
+		
+		Game2ButtonText.text = "Game 2 (Selected)";
+		Game1ButtonText.text = "Game 1";
 	}
 
 	public void ButtonPlayer1() {
@@ -49,6 +77,8 @@ public class PRototyping : MonoBehaviour {
 			ActiveGame = Game1;
 		}
 		SelectedPlayer = ActiveGame.Player1;
+		PlayButtonText.text = "Play as Player 1!";
+		PlayButton.SetActive(true);
 
 	}
 
@@ -57,6 +87,8 @@ public class PRototyping : MonoBehaviour {
 			ActiveGame = Game1;
 		}
 		SelectedPlayer = ActiveGame.Player2;
+		PlayButtonText.text = "Play as Player 2!";
+		PlayButton.SetActive(true);
 	}
 
 	public void ButtonPlayer3() {
@@ -64,6 +96,8 @@ public class PRototyping : MonoBehaviour {
 			ActiveGame = Game1;
 		}
 		SelectedPlayer = ActiveGame.Player3;
+		PlayButtonText.text = "Play as Player 3!";
+		PlayButton.SetActive(true);
 	}
 
 	public void ButtonPlayer4() {
@@ -71,8 +105,10 @@ public class PRototyping : MonoBehaviour {
 			ActiveGame = Game1;
 		}
 		SelectedPlayer = ActiveGame.Player4;
+		PlayButtonText.text = "Play as Player 4!";
+		PlayButton.SetActive(true);
 	}
-
+	#endregion
 	public void SetPlayerScreen() {
 		MainMenu.SetActive (false);
 		for(int i=0; i<Activities.Length; i++) {
@@ -87,10 +123,18 @@ public class PRototyping : MonoBehaviour {
 			Items[i].text = SelectedPlayer.Items[i];
 		}
 
-		for(int i=0; i<Rules.Length; i++) {
-			Rules[i].text = SelectedPlayer.Rules[i];
-		}
+		Rules[0].text = SelectedPlayer.Rules[0];
 
 		PlayerInfo.SetActive (true);
+	}
+
+	public void ShowNextRule() {
+		iCurrentRules++;
+		Rules[iCurrentRules].text = SelectedPlayer.Rules[iCurrentRules];
+	}
+
+	public void EndGame () {
+		PlayerInfo.SetActive(false);
+		EndScreen.SetActive(true);
 	}
 }
--- PapercutStudios/Assets/_Scripts/Timer.cs
+++ PapercutStudios/Assets/_Scripts/Timer.cs
@@ -6,25 +6,54 @@ public class Timer : MonoBehaviour {
 
 	public Text timerLabel;
 	
-	public float CountDownTime;
-	// Use this for initialization
-	void Start () {
+	public float StartingCountDownTime;
+	public float fCountDownTimer;
+	private PRototyping UICommands;
+	private float fInterval;
+	private bool bTimerRunning;
+	private bool ShownRule2;
+	private bool ShownRule3;
 
+	void Awake() {
+		UICommands = FindObjectOfType<PRototyping>();
 	}
-	
-	// Update is called once per frame
+
+	void Start () { 
+		fCountDownTimer = StartingCountDownTime;
+		fInterval = fCountDownTimer/3;
+		bTimerRunning = true;
+		ShownRule2 = false;
+		ShownRule3 = false;
+	}
+
 	void Update () {
-		CountDownTime -= Time.deltaTime;
-		
-		var minutes = CountDownTime / 60;
-		var seconds = CountDownTime % 60;
-		//var fraction = (CountDownTime * 100) % 100;
+		if(bTimerRunning) {
+			fCountDownTimer -= Time.deltaTime;
+			
+			float minutes = fCountDownTimer / 60;
+			float seconds = fCountDownTimer % 60;
+			//var fraction = (CountDownTime * 100) % 100;
 
-		timerLabel.text = string.Format ("{0:00} : {1:00}", minutes, seconds);
+			timerLabel.text = string.Format ("{0:00} : {1:00}", minutes, seconds);
 
-		if (CountDownTime <= 0.0f) 
-		{
-			Application.LoadLevel(1);
+			if (fCountDownTimer <= StartingCountDownTime - fInterval && !ShownRule2) {
+				UICommands.ShowNextRule();
+				ShownRule2 = true;
+			}
+
+			if(fCountDownTimer <= StartingCountDownTime - (2*fInterval) && !ShownRule3) {
+				UICommands.ShowNextRule();
+				ShownRule3 = true;
+			}
+
+			if (fCountDownTimer <= 0.0f) 
+			{
+				UICommands.EndGame();
+			}
 		}
+	}
+
+	public void StopTimer () {
+		bTimerRunning = false;
 	}
 }
