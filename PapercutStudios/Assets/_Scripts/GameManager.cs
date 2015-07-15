e8a36855dd94e6e47a896b26ed660858099bb04a
unknown <lbrown53@SYD-0525-W.sae.au>
2015-07-15 15:38:17 +1000

    Beginnings

---
+++ PapercutStudios/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	PlayerTable ptPlayer1 = new PlayerTable();
+	PlayerTable ptPlayer2 = new PlayerTable();
+	PlayerTable ptPlayer3 = new PlayerTable();
+	PlayerTable ptPlayer4 = new PlayerTable();
+
+	// Use this for initialization
+	void Start () {
+		ptPlayer1.ActivityInfo[0,0] = 2;
+		ptPlayer1.AvailabilityInfo[0,0] = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ PapercutStudios/Assets/_Scripts/PlayerTable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTable {
+
+	public int[,] ActivityInfo = new int[4,5];
+
+	public int[,] AvailabilityInfo = new int[2,5];
+
+	public int AnswerHours1;
+
+	public int AnswerHours2;
+}
