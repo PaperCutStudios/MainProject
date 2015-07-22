51bcecdbd1f3267f1fb9e27ccefd594d4c2c8ff8
unknown <lbrown53@SYD-0525-W.sae.au>
2015-07-22 14:59:47 +1000

    Added Game Manager Logic, basic XMLReaders

--- PapercutStudios/Assets/_Scripts/GameManager.cs
+++ PapercutStudios/Assets/_Scripts/GameManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System;
 
 public class GameManager : MonoBehaviour {
 
@@ -8,6 +10,19 @@ public class GameManager : MonoBehaviour {
 	PlayerTable ptPlayer3 = new PlayerTable();
 	PlayerTable ptPlayer4 = new PlayerTable();
 
+	PlayerTable activeTable = new PlayerTable();
+
+	public int iActiveTable;
+
+	public int iBracketTime = 3;
+
+	public Text[] Activities;
+	public Text[] Availabilities;
+	public Text[] Items;
+	public Text[] Rules;
+
+	XMLInformationReader xmlInformationReader;
+
 	// Use this for initialization
 	void Start () {
 		ptPlayer1.ActivityInfo = WriteActivityInfo (2, 0, 6, 8, 1, 0, 1, 3, 2, 1, 3, 5, 6, 3, 4, 1, 0, 14, 6, 0);
@@ -15,17 +30,111 @@ public class GameManager : MonoBehaviour {
 		ptPlayer3.ActivityInfo = WriteActivityInfo (0, 5, 1, 2, 3, 1, 0, 1, 0, 2, 5, 6, 4, 3, 4, 0, 8, 0, 1, 7);
 		ptPlayer4.ActivityInfo = WriteActivityInfo (3, 6, 1, 2, 0, 4, 6, 1, 0, 1, 5, 2, 4, 3, 5, 11, 5, 0, 1, 0);
 
-		ptPlayer1.AvailabilityInfo = WriteAvailabilityInfo (0, 2, 5, 4, 1, 0, 4, 8, 17, 1);
-		ptPlayer2.AvailabilityInfo = WriteAvailabilityInfo (0, 5, 2, 6, 1, 0, 12, 5, 3, 1);
-		ptPlayer3.AvailabilityInfo = WriteAvailabilityInfo (0, 3, 2, 6, 1, 0, 9, 2, 11, 1);
-		ptPlayer4.AvailabilityInfo = WriteAvailabilityInfo (0, 2, 4, 5, 1, 0, 3, 4, 12, 1);
+		ptPlayer1.AvailabilityInfo = WriteAvailabilityInfo (5, 2, 0, 4, 1, 8, 4, 0, 17, 1);
+		ptPlayer2.AvailabilityInfo = WriteAvailabilityInfo (0, 5, 2, 1, 6, 0, 12, 5, 1, 3);
+		ptPlayer3.AvailabilityInfo = WriteAvailabilityInfo (1, 3, 2, 6, 0, 4, 9, 2, 11, 0);
+		ptPlayer4.AvailabilityInfo = WriteAvailabilityInfo (4, 2, 0, 5, 1, 7, 3, 0, 12, 1);
+
+		switch(iActiveTable) {
+		case 1 :
+			activeTable = ptPlayer1;
+			break;
+		case 2 :
+			activeTable = ptPlayer2;
+			break;
+		case 3 :
+			activeTable = ptPlayer3;
+			break;
+		case 4 :
+			activeTable = ptPlayer4;
+			break;
+		default :
+			activeTable = ptPlayer1;
+			Debug.Log("Default Case");
+			break;
+		}
+
+
+
+		for (int i = 0; i<activeTable.ActivityInfo.GetLength(1); i++) {
+			if(activeTable.ActivityInfo[0,i] = 0) {
+				Activities[i].text = xmlInformationReader.GetActivityPiece(activeTable.ActivityInfo[0,i]) + " " +
+								xmlInformationReader.GetDayPiece(activeTable.ActivityInfo[1,i]) + " " +
+								xmlInformationReader.GetDayPiece(activeTable.ActivityInfo[2,i]) + " " +
+						        CalculateOpenHours(xmlInformationReader.GetTimePiece(activeTable.ActivityInfo[3,i]), true);
+			}
+			else {
+				Activities[i].text = xmlInformationReader.GetActivityPiece(activeTable.ActivityInfo[0,i]) + " " +
+								xmlInformationReader.GetDayPiece(activeTable.ActivityInfo[1,i]) + " " +
+								xmlInformationReader.GetDayPiece(activeTable.ActivityInfo[2,i]) + " " +
+						        CalculateOpenHours(xmlInformationReader.GetTimePiece(activeTable.ActivityInfo[3,i]), false);
+			}
+		}
+		for (int i = 0; i<activeTable.AvailabilityInfo.GetLength(1); i++) {
+			Availabilities[i].text = xmlInformationReader.GetDayPiece(activeTable.AvailabilityInfo[0,i]) + " " +
+									CalculateAvailabilityHours(xmlInformationReader.GetTimePiece(activeTable.AvailabilityInfo[1,i]));
+		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Activities[0].text =  xmlInformationReader.GetActivityPiece(activeTable.ActivityInfo[0,0]);
 	
 	}
 
+	string CalculateAvailabilityHours(string input) {
+		string sOutputString;
+		int iRootNum;
+		int iBegin;
+		int iEnd;
+		Random rand = new Random();
+
+		iEnd = rand.Next(iRootNum, iRootNum + iBracketTime);
+		iBegin = iEnd-iBracketTime;
+		sOutputString = iBegin.ToString() + " til " + iEnd.ToString()
+	}
+
+	string CalculateOpenHours(string input, bool IsAnswer) {
+		string OutputString;
+		string sOpen;
+		string sClosed;
+		int iRootNum;
+		if(int.TryParse(input, out iRootNum)) {
+			if(iRootNum <12 && IsAnswer) {
+				sOpen = (iRootNum - iBracketTime).ToString();
+				sClosed = ((iRootNum - iBracketTime) + 8).ToString();
+				OutputString = sOpen + " - " + sClosed;
+			}
+			else if (iRootNum <12 && !IsAnswer) {
+				sOpen = (iRootNum + iBracketTime + 1).ToString();
+				sClosed = (iRootNum + iBracketTime + 1 + 8).ToString();
+				OutputString = sOpen + " - " + sClosed;
+			}
+			else if (iRootNum >= 12 && IsAnswer) {
+				sClosed = (iRootNum + iBracketTime).ToString();
+				sOpen = ((iRootNum + iBracketTime) - 8).ToString();
+				OutputString = sOpen + " - " + sClosed;
+			}
+			else if (iRootNum >= 12 && !IsAnswer) {
+				sClosed = (iRootNum - iBracketTime).ToString();
+				sOpen = ((iRootNum - iBracketTime) - 8).ToString();
+				OutputString = sOpen + " - " + sClosed;
+			}
+			else {
+				OutputString = "Wrong Times";
+				Debug.Log("Oh Shit, something slipped through Opening Hours");
+			}
+		}
+		else {
+			OutputString = "Couldn't Parse";
+			Debug.Log("Could not parse Open Hours");
+		}
+		return OutputString;
+	}
+
+
+	#region
 	int[,] WriteActivityInfo( int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t)
 	{
 		int[,] Output = new int[4, 5];
@@ -69,4 +178,5 @@ public class GameManager : MonoBehaviour {
 
 		return Output;
 	}
+	#endregion
 }
---
+++ PapercutStudios/Assets/_Scripts/XMLInformationReader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XMLInformationReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public string GetActivityPiece(int i) {
+		//compare int i to activities in information xml
+	}
+
+	public string GetDayPiece(int i) {
+
+	}
+
+	public string GetTimePiece(int i) {
+
+	}
+}
