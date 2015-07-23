0502f1736947386a13c4f1745ef2ae8cfce22e1c
unknown <lbrown53@SYD-1241-W.sae.au>
2015-07-23 11:42:13 +1000

    Setting up some net stuff

---
+++ PapercutStudios/Assets/_Scripts/TCPClientManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.IO;
+
+
+public class TCPClientManager : MonoBehaviour {
+
+	TcpClient tcpClient;
+	Stream stm;
+
+	// Use this for initialization
+	void Start () {
+		tcpClient = new TcpClient();
+		try {
+			tcpClient.Connect("192.168.0.1",80);
+			stm = tcpClient.GetStream();
+
+		}
+		catch {
+			Debug.Log("Could not connect");
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
