3d907ac7855e55b01cc261ed11e4669bdffb97ed
Lachlan Brown <lachlan.brown8@live.com.au>
2015-07-31 19:17:34 +1000

    Removing some unnecessary files

--- PapercutStudios/Assets/_Scripts/Prototype/PlayerTemplate.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-public class PlayerTemplate {
-	public string[] Activities;
-	public string[] Availabilities;
-	public string[] Items;
-	public string[] Rules;
-}
