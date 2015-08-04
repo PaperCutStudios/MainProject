e56edb5c647abac7ca72993cbf22980c1b563ea6
Mackenzie Allanson <111294@student.sae.edu.au>
2015-08-04 21:13:56 +1000

    Start of Excel work

---
+++ PapercutStudios/Assets/_Scripts/ExcelReader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System; 
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel; 
+
+
+public class ExcelReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ReadXLSX (Application.dataPath + "/Excel/TestAnswer.xlsx");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void ReadXLSX(string filetoread)
+	{
+
+	}
+}
