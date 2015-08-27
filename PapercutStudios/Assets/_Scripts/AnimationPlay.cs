d4d89ce3785af0c92478e3460afce5f26dda4336
unknown <mallanson@SYD-1253-W.sae.au>
2015-08-27 11:02:30 +1000

    Animation Added for main screen transition

---
+++ PapercutStudios/Assets/_Scripts/AnimationPlay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationPlay : MonoBehaviour {
+
+	public GameObject Anim;
+	// Use this for initialization
+	void Start () {
+		Anim.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void AnimationActive(){
+		Anim.gameObject.SetActive (true);
+	}
+}
